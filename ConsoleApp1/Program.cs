075ec7e327354d5e5cf86ebb609b61ca3f59e1a3
Dream Wood <VladGggolubev2015@gmail.com>
2023-10-18 13:30:28 +0300

    Add select menu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,31 @@
 ﻿using ConsoleApp1;
 
-IProgramModule module = new Za7();
+IProgramModule module = new Za1();
 
-module.Run();
+while (true)
+{
+    Console.WriteLine($"Выберите задание: (1,2,3) ЛР2 и (4,5,6) ЛР 3");
+    
+    if (!int.TryParse(Console.ReadLine(), out int num)) break;
+    
+    switch (num)
+    {
+        case 1:
+            module = new Za1(); break;
+        case 2:
+            module = new Za2(); break;
+        case 3:
+            module = new Za3(); break;
+        case 4:
+            module = new Za4(); break;
+        case 5:
+            module = new Za5(); break;
+        case 6:
+            module = new Za6(); break;
+        default:
+            Console.WriteLine("Нет такого номера!");
+            continue;
+    }
+
+    module.Run();
+}
--- ConsoleApp1/Za7.cs
+++ ConsoleApp1/Za7.cs
@@ -1,22 +1,22 @@
 namespace ConsoleApp1;
 
-public class Za7: IProgramModule
+public class Za7 : IProgramModule
 {
     public void Run()
     {
-
-        double a = double.Parse(Console.ReadLine() ?? string.Empty);
-        double b = double.Parse(Console.ReadLine() ?? string.Empty);
-        double r = double.Parse(Console.ReadLine() ?? string.Empty);
-        double d = double.Parse(Console.ReadLine() ?? string.Empty);
-        double e = double.Parse(Console.ReadLine() ?? string.Empty);
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double r = double.Parse(Console.ReadLine());
+        double d = double.Parse(Console.ReadLine());
+        double e = double.Parse(Console.ReadLine());
 
         // (x-a)^2 + (y-b)^2 = r^2
         // y = dx + e
 
-        double core = 2 * Math.Sqrt(-2*a*d*e + 2*a*d*b - Math.Pow(e, 2) - Math.Pow(b, 2) + 2*e*b + Math.Pow(r,2) - Math.Pow(d,2) * Math.Pow(a,2) + Math.Pow(d,2)*Math.Pow(r,2));
-        double x1 = (2*a-2*d*e+2*d*b + core) / (2+2*Math.Pow(d,2));
-        double x2 = (2*a-2*d*e+2*d*b - core) / (2+2*Math.Pow(d,2));
+        double core = 2 * Math.Sqrt(-2 * a * d * e + 2 * a * d * b - Math.Pow(e, 2) - Math.Pow(b, 2) + 2 * e * b +
+            Math.Pow(r, 2) - Math.Pow(d, 2) * Math.Pow(a, 2) + Math.Pow(d, 2) * Math.Pow(r, 2));
+        double x1 = (2 * a - 2 * d * e + 2 * d * b + core) / (2 + 2 * Math.Pow(d, 2));
+        double x2 = (2 * a - 2 * d * e + 2 * d * b - core) / (2 + 2 * Math.Pow(d, 2));
 
         double y1 = d * x1 + e;
         double y2 = d * x2 + e;
@@ -34,6 +34,5 @@ public class Za7: IProgramModule
             Console.WriteLine(x1 + " " + y1);
             Console.WriteLine(x2 + " " + y2);
         }
-
     }
 }
